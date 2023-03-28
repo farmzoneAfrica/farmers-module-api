668f0631bdbf0acd7087f8668204e89a8fe388f3
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2023-03-28 02:33:05 +0100

    test url

--- app/Http/Controllers/Auth/FarmerRegisterController.php
+++ app/Http/Controllers/Auth/FarmerRegisterController.php
@@ -69,7 +69,7 @@ class FarmerRegisterController extends BaseController
     public function index(FarmerRegisterRequest $request, FarmerRegisterServices $services): JsonResponse
     {
         return $services->register($request);
-        //test
+        ///test
     }
 
     /**
