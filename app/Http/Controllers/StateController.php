f85fbb8da47cf0eba678aab5f9de9948c8eede41
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2023-04-19 05:01:35 +0100

    swagger grouping

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 
 class StateController extends BaseController
 {
-
     /**
      * Get List of States
      * @OA\Get (
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('migrate', function () {
+    Artisan::command('migrate');
+});
+
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
