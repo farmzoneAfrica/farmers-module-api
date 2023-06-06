b132dec518099ab2cbca3cba69b90dc1c31fe991
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2023-06-06 14:08:42 +0100

    rebase

--- routes/auth.php
+++ routes/auth.php
@@ -27,11 +27,7 @@ Route::prefix('farmer')->group(function () {
     Route::post('forgot-pin', \App\Http\Controllers\Auth\Farmer\ForgotPinController::class)->middleware('guest')->name('forgot.pin');
     Route::post('verify-forgot-pin-otp', \App\Http\Controllers\Auth\Farmer\VerifyForgotPinOtpController::class)->middleware(['auth:sanctum', 'forgot.pin.access']);
     Route::put('reset-pin', \App\Http\Controllers\Auth\Farmer\ResetPinController::class)->middleware(['auth:sanctum', 'forgot.pin.access']);
-    Route::get('logout', \App\Http\Controllers\Auth\Farmer\LoginController::class)->middleware('auth:sanctum')->name('farmer.logout.pin');
-
-
-
-
+    Route::get('logout', \App\Http\Controllers\Auth\Farmer\LogoutController::class)->middleware('auth:sanctum')->name('farmer.logout.pin');
 
     Route::get('login', function () {
         return response()->json(['message'=>'Unauthenticated'], 401);
